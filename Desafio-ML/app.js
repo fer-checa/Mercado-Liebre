2cf3e650a84eae8fa72fd6ab006bb3d35e9c55e7
fer-checa <fernanda.checa2015@gmail.com>
2022-05-04 01:47:00 -0300

    Registracion y Login

--- Desafio-ML/app.js
+++ Desafio-ML/app.js
@@ -19,9 +19,21 @@ app.listen(3030, () => {
     console.log("Servidor corriendo en http://localhost:3030")
 });
 
-// Definimos las rutas a los distintos pedidos que nuestro sitio sabe responder
+// Definimos las rutas a los distintos pedidos que nuestro sitio 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
-})
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+app.get("/", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+app.get("/", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+
+
+
+
+
 
 // NO OLVIDAR LEVANTAR EL PROYECTO CON: npm start
