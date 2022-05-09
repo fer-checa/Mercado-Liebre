4bcd8a99af43692ac44f992989045e6baa9e5fef
fer-checa <fernanda.checa2015@gmail.com>
2022-05-09 00:56:14 -0300

    Login y Register

--- Desafio-ML/app.js
+++ Desafio-ML/app.js
@@ -20,20 +20,20 @@ app.listen(3030, () => {
 });
 
 // Definimos las rutas a los distintos pedidos que nuestro sitio 
-/* app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
-app.get('/', (req,res) => {
+app.get('/register', (req,res) => {
     res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
-app.get('/', (req,res) => {
+app.get('/login', (req,res) => {
     res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });  
- */
+ 
 // NO OLVIDAR LEVANTAR EL PROYECTO CON: npm start
 
 
-app.get('/', (req,res) => {
+/* app.get('/', (req,res) => {
     let htmlPath = path.resolve(__dirname, './views/home.html');
     res.sendFile(htmlPath);
 });
@@ -44,4 +44,4 @@ app.get('/register', (req,res) => {
 app.get('/login', (req,res) => {
     let htmlPath = path.resolve(__dirname, './views/login.html');
     res.sendFile(htmlPath);
-})   
+})   */ 
